f921838272d2caefb293f06a6c5fff09b3caea2f
Luca Chilan <lucachilan@gmail.com>
2026-05-14 22:58:54 -0300

    Implementing details about products for week2

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,20 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
+const productID = getParam("product");
 
-console.log(dataSource.findProductById(productId))
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
